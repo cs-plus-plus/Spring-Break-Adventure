81a2f173e4d5060bf3254e45e8e3c27d6dde72bc
CS++ <kevin@csplusplus.com>
2025-04-07 10:23:27 -0700

    Update Item.java

--- src/Item.java
+++ src/Item.java
@@ -1,4 +1,10 @@
 
 public class Item {
+    String name;
+    String description;
+    String effect;
 
+    public void use(Player player, Character character) {
+        // Define what the item does
+    }
 }
