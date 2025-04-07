313b7a9f5694c1f0ee518e13be0ca7342f3d6a5f
CS++ <kevin@csplusplus.com>
2025-04-07 10:23:57 -0700

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,15 @@
 
 public class Player {
+    int energy = 100;
+    ArrayList<Item> inventory = new ArrayList<>();
+    Location currentLocation;
 
+    public void pickUpItem(Item item) {
+        inventory.add(item);
+    }
+
+    public void travel(Location nextLocation) {
+        currentLocation = nextLocation;
+        currentLocation.enterLocation();
+    }
 }
