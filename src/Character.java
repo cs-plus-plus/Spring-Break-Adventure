cae1873db8fac937bc4e2c7466eb7eae4771accf
CS++ <kevin@csplusplus.com>
2025-04-07 10:24:39 -0700

    Update Character.java

---
+++ src/Character.java
@@ -0,0 +1,10 @@
+
+public class Character {
+    String name;
+    int energy;
+    String description;
+
+    public void interact(Player player) {
+        // Affect player's energy or provide clues
+    }
+}
